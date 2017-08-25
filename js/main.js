c891dda5ca4cc6a590e7e37fde5d732bb0c737e6
Matthew Gosline <matt.gosline@mapbox.com>
2017-08-24 20:20:12 -0700

    updated files

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 // Make it rain!
+
 // var currentImageNumber = 0;
 // When the user clicks the next button (#next)
 // $('button').on('click', function() {
-  // if (currentImageNumber < 1) {
-  //   currentImageNumber += 1;
-  //   console.log(currentImageNumber);
-  // } else {
-  //   currentImageNumber = 0;
-  //   console.log(currentImageNumber);
-  // }
-  // $(this).prevAll('.carousel-wrapper').find('img').hide();
-//   $(this).prevAll('.carousel-wrapper').find('img').toggle();
-//
+//   if (currentImageNumber < 1) {
+//     currentImageNumber += 1;
+//     console.log(currentImageNumber);
+//   } else {
+//     currentImageNumber = 0;
+//     console.log(currentImageNumber);
+//   }
+
+  $(this).prevAll('.carousel-wrapper').find('img').hide();
+  $(this).prevAll('.carousel-wrapper').find('img').toggle();
+
 // });
 
 
 
-$('.click-zone-left, .click-zone-right, .controls').on('click ', function(event) {
+$('.click-zone-left, .click-zone-right, .pagination-dots').on('click ', function(event) {
   event.preventDefault();
   $(this).prevAll('.carousel-wrapper').find('img').toggle();
+  $(".pagination-dot").toggleClass("active");
 });
 
 
@@ -26,4 +29,13 @@ $('.click-zone-left, .click-zone-right, .controls').on('click ', function(event)
 $('.carousel-wrapper img').on('swipeleft swiperight', function() {
   $(this).siblings().toggle();
   $(this).toggle();
+});
+
+$('.hamburger').on('click', function () {
+  // if ($('.menu').hasClass('open')) {
+  //   $('.menu').removeClass('open');
+  // } else {
+  //   $('.menu').addClass('open');
+  // }
+$('.menu').toggleClass('open');
 });
